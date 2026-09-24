b8077380d007de9a75fde9c0c18befd234b2f071
Chloe Tan <chloe.faith.tan@gmail.com>
2026-09-24 18:48:44 -0400

    removed error checking and updated tests

--- pricing.py
+++ pricing.py
@@ -1,17 +1,20 @@
+import numpy as np
+
 def calculate_price (flight):
     # calculate factors
+    base_fare = flight.base_fare_cad
     time_factor = calculate_time_factor(flight)
     demand_factor = flight.demand_score
     capacity_factor = calculate_capacity_factor(flight)
     seasonal_factor = calculate_seasonal_factor(flight)
     weekend_factor = calculate_weekend_factor(flight)
 
-    # initialize variables
-    min_price = 45
-    max_price = 1500
-
     # calculate price
-    price = flight.base_fare_cad * time_factor * demand_factor * capacity_factor * seasonal_factor * weekend_factor
+    price = base_fare * time_factor * demand_factor * capacity_factor * seasonal_factor * weekend_factor
+
+    # set min and max price
+    min_price = base_fare * 0.7
+    max_price = base_fare * 3.0
 
     # check min and max price
     if price < min_price:
@@ -19,61 +22,45 @@ def calculate_price (flight):
     elif price > max_price:
         price = max_price
 
-    return (price)
+    return round(price, 2)
+
 
 def calculate_time_factor (flight):
     days_until_departure = flight.days_to_departure()
-    if (days_until_departure < 0):
-        raise ValueError("Days to departure must be greater than or equal to 0")
     
-    if days_until_departure <= 7:
-            time_factor = 1.35
-    elif 8 <=days_until_departure <= 21:
-        time_factor = 1.10
-    else:
-        time_factor = 1.00
+    # construct a sigmoid curve to calculate time factor
+    max_premium = 0.5
+    k = 0.35
+    t0 = 7
+    time_factor = 1 + (max_premium) / (1 + np.exp(k * (days_until_departure - t0)))
 
-    return (time_factor)
+    return round(time_factor, 2)
+
 
 def calculate_capacity_factor (flight):
-    # error checking
-    if (flight.seats_remaining <= 0):
-         raise ValueError("Seats remaining must be greater than 0")
-    if (flight.capacity <= 0):
-        raise ValueError("Capacity must be greater than 0")
-    if (flight.capacity < flight.seats_remaining):
-        raise ValueError("Capacity must be greater than seats remaining")
-
     load_factor = 1 - (flight.seats_remaining / flight.capacity)
     capacity_factor = 1 + 0.45 * load_factor
 
     return(capacity_factor)
 
+
 def calculate_seasonal_factor (flight):
-    # error checking
-    if (flight.season != "peak" and flight.season != "shoulder" and flight.season != "regular"):
-        raise ValueError("Season must be peak, shoulder, or regular")
+    season = flight.season.lower()
     
-    if (flight.season == "peak"):
+    if (season == "peak"):
         seasonal_factor = 1.43
-    elif (flight.season == "shoulder"):
+    elif (season == "shoulder"):
         seasonal_factor = 1.18
     else:
         seasonal_factor = 1
 
     return (seasonal_factor)
 
+
 def calculate_weekend_factor (flight):
-    # error checking
-    if (not(isinstance(flight.is_weekend, bool))):
-        raise TypeError("Weekend must be True or False") 
-    
     if (flight.is_weekend == True):
         weekend_factor = 1.13
     else:
         weekend_factor = 1
 
     return (weekend_factor)
-
-
-
--- test_pricing.py
+++ test_pricing.py
@@ -2,44 +2,17 @@ from datetime import date, timedelta
 from flight import Flight
 from pricing import *
 
-# test minimum fare
-flight1 = Flight(flight_id="AC123",origin="YYZ",destination="YVR",departure_date="2028-09-25",
-                departure_time="08:30",base_fare_cad=1,capacity=100,seats_remaining=12,
-                demand_score=1.32,demand_level="high",season="peak",is_weekend=False)
-
-assert calculate_price(flight1) == 45
-
 # test maximum fare
+flight1 = Flight(flight_id="AC123",origin="YYZ",destination="YVR",departure_date="2028-09-25",
+                departure_time="08:30",base_fare_cad=100,capacity=100,seats_remaining=100,
+                demand_score=3.1,demand_level="high",season="regular",is_weekend=False)
+
+assert calculate_price(flight1) == 300
+
+# test minimum fare
 flight2 = Flight(flight_id="AC123",origin="YYZ",destination="YVR",departure_date="2028-09-25",
-                departure_time="08:30",base_fare_cad=2000,capacity=100,seats_remaining=12,
-                demand_score=1.32,demand_level="high",season="peak",is_weekend=False)
+                departure_time="08:30",base_fare_cad=100,capacity=100,seats_remaining=100,
+                demand_score=0.6,demand_level="high",season="regular",is_weekend=False)
 
-assert calculate_price(flight2) == 1500
+assert calculate_price(flight2) == 70
 
-# test flights where departure time has passed
-flight3 = Flight(flight_id="AC123",origin="YYZ",destination="YVR",departure_date="2026-09-20",
-                departure_time="08:30",base_fare_cad=250,capacity=100,seats_remaining=12,
-                demand_score=1.32,demand_level="high",season="peak",is_weekend=False)
-
-calculate_price(flight3)
-
-# test seats remaining > capacity 
-flight4 = Flight(flight_id="AC123",origin="YYZ",destination="YVR",departure_date="2028-09-25",
-                departure_time="08:30",base_fare_cad=250,capacity=10,seats_remaining=120,
-                demand_score=1.32,demand_level="high",season="peak",is_weekend=False)
-
-calculate_price(flight4)
-
-# test seats remaining <= 0 
-flight5 = Flight(flight_id="AC123",origin="YYZ",destination="YVR",departure_date="2028-09-25",
-                departure_time="08:30",base_fare_cad=250,capacity=10,seats_remaining=0,
-                demand_score=1.32,demand_level="high",season="peak",is_weekend=False)
-
-calculate_price(flight5)
-
-# test capacity <= 0 
-flight6 = Flight(flight_id="AC123",origin="YYZ",destination="YVR",departure_date="2028-09-25",
-                departure_time="08:30",base_fare_cad=250,capacity=0,seats_remaining=10,
-                demand_score=1.32,demand_level="high",season="peak",is_weekend=False)
-
-calculate_price(flight6)
